5449aa44039bb2489b8c8d1d8d124dd48d55eca9
ngoc <ngochc1@gmail.com>
2023-12-14 20:12:43 +0700

    add list trending gifs

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GiphyService } from '../../core/services/giphy.service';
-
+import { renderGif, renderGrid } from '@giphy/js-components';
+import { GiphyFetch } from '@giphy/js-fetch-api'
+import { throttle } from 'throttle-debounce';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  offset = 0;
+  gifs: any = [];
   constructor(private giphyService: GiphyService) {
-    console.log(giphyService.fetchGifs(0));
+  }
+  vanillaJSGif = async (gif: any, mountNode: HTMLElement,) => {
+    // render a single gif
+    renderGif({ gif: gif, width: 300 }, mountNode)
+  }
+  makeGrid = (targetEl: HTMLElement) => {
+    const render = () => {
+      // here is the @giphy/js-components import
+      return renderGrid(
+        {
+          width: innerWidth,
+          fetchGifs: this.giphyService.fetchGifs,
+          columns: innerWidth < 1000 ? 2 : innerWidth > 1500 ? 4 : 3,
+          gutter: 6,
+          noLink: true,
+          onGifClick: (gif: any) => {
+            console.log(gif)
+          },
+        },
+        targetEl
+      )
+    }
+    const resizeRender = throttle(500, render)
+    window.addEventListener('resize', resizeRender, false)
+    const remove = render()
+    return {
+      remove: () => {
+        remove()
+        window.removeEventListener('resize', resizeRender, false)
+      },
+    }
+  }
+  async ngOnInit(): Promise<void> {
+    this.gifs = (await this.giphyService.fetchGifs(this.offset)).data;
+    this.offset += 10;
+    console.log(this.gifs)
+    // this.vanillaJSGif(this.gifs[0], document.getElementById('gif')!)
+    this.makeGrid(document.getElementById('gif')!)
   }
 }
